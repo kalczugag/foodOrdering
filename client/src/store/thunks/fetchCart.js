dc2f16aff975dca74d3e44f6f81667f92feda6ec
Gabriel Kałczuga <kalczugag@gmail.com>
2023-08-07 14:16:15 +0200

    console.log remove

--- client/src/store/thunks/fetchCart.js
+++ client/src/store/thunks/fetchCart.js
@@ -4,6 +4,5 @@ import axios from "axios";
 export const fetchCart = createAsyncThunk("cart/fetch", async () => {
     const response = await axios.get("/api/cart");
 
-    console.log(response.data);
     return response.data;
 });
