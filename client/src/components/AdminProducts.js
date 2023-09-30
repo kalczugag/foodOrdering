dfd7e975b6daecda984765f891273c23f918ea87
kalczugag <kalczugag@gmail.com>
2023-09-30 14:08:24 +0200

    remove React from import

--- client/src/components/AdminProducts.js
+++ client/src/components/AdminProducts.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useThunk } from "../hooks/use-thunk";
 import { removeProduct } from "../store";
