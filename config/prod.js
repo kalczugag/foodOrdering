7a82fa8b18d6494e061f219a4d70806658ffe6cd
kalczugag <kalczugag@gmail.com>
2023-10-02 12:37:36 +0200

    prod keys fix

--- config/prod.js
+++ config/prod.js
@@ -1,13 +1,14 @@
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    googleProjectId: process.env.GOOGLE_STORAGE_PROJECT_ID,
+    googleBucketName: process.env.GOOGLE_STORAGE_BUCKET_NAME,
+    googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
     stripeSecretKey: process.env.STRIPE_SECRET_KEY,
     stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
     stripeEndpointSecret: process.env.STRIPE_ENDPOINT_SECRET,
     redirectDomain: process.env.REDIRECT_DOMAIN,
-    googleProjectId: process.env.GOOGLE_STORAGE_PROJECT_ID,
-    googleBucketName: process.env.GOOGLE_STORAGE_BUCKET_NAME,
-    googleCredentials: process.env.GOOGLE_CREDENTIALS,
 };
