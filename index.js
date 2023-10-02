24244730fdd474530ffa6637c8c629a80f4188f0
kalczugag <kalczugag@gmail.com>
2023-10-02 14:01:23 +0200

    server timeout to 60s

--- index.js
+++ index.js
@@ -54,6 +54,8 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+const server = app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
 
-//session regenerate error
+server.timeout = 60000;
